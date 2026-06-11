f19726054973b571edd1ce92c7778726348d798b
Inn <dulsfnj413@naver.com>
2026-06-11 15:27:04 +0900

    prob2 del annot

--- day2/problem2.js
+++ day2/problem2.js
@@ -22,20 +22,3 @@ console.log(sumBetween("1", "5"));
 console.log(sumBetween("3", "3"));
 console.log(sumBetween("5", "1"));
 console.log(sumBetween("1.5", "3"));
-
-
-/*
-### 2번. `sumBetween(startValue, endValue)` 함수를 만듭니다.
-
-- 두 값을 숫자로 바꿉니다.
-- 둘 중 하나라도 숫자로 바꿀 수 없거나 정수가 아니면 `0`을 돌려줍니다.
-- 시작값이 끝값보다 크면 `0`을 돌려줍니다.
-- 시작값부터 끝값까지의 합계를 돌려줍니다.
-
-확인할 값입니다.
-
-- `sumBetween("1", "5")`의 결과는 `15`입니다.
-- `sumBetween("3", "3")`의 결과는 `3`입니다.
-- `sumBetween("5", "1")`의 결과는 `0`입니다.
-- `sumBetween("1.5", "3")`의 결과는 `0`입니다.
-*/
